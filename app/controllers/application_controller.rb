95e7c4fffd21094029ecc9d9307063bc29e85b45
Weijia Li <unicar9@gmail.com>
2017-07-29 15:14:18 +1000

    users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :fetch_user
+
+  private
+  def fetch_user
+    @current_user = User.find_by id:session[:user_id] if session[:user_id].present?
+    session[:user_id]=nil unless @current_user.present?
+  end
+
+  def check_if_logged_in
+    redirect_to login_path unless @current_user.present?
+  end
+
+  def check_if_admin
+    redirect_to login_path unless @current_user.present? && @current_user.is_admin?
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,36 @@
 class UsersController < ApplicationController
+  before_action :check_if_logged_in, only: [:edit, :update, :destroy]
+  before_action :check_if_admin, only: [:index]
+  before_action :get_user, only: [:show, :edit, :update]
+
   def index
+    @users = User.all
   end
 
   def show
   end
 
   def new
+    @user = User.new
   end
 
   def create
   end
 
   def edit
+    redirect_to root_path unless @current_user == @user
   end
 
   def update
   end
 
   def destroy
+    @user.destroy
+  end
+
+  private
+
+  def get_user
+      @user = User.find params["id"]
   end
 end
