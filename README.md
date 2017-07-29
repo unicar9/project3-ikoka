27294b750eb365b0529241e8abc89051fa9a9aad
Lingxiao Wang <xwangl213@gmail.com>
2017-07-29 16:21:09 +1000

    cloudinary working, user index show added

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # README
 
+iKoka 爱好家
+
 This README would normally document whatever steps are necessary to get the
 application up and running.
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,4 +54,8 @@ class UsersController < ApplicationController
   def get_user
       @user = User.find params["id"]
   end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :avatar, :password, :password_confirmation)
+  end
 end
