079c128671fe80c311c68294309f7f7ecbd3652d
Lingxiao Wang <xwangl213@gmail.com>
2017-08-03 16:39:03 +1000

    Merge branch 'p5v2' of https://github.com/unicar9/project3-ikoka into p5v2

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,7 +1,6 @@
-require "pry"
 
 class ChatroomsController < ApplicationController
-  # before_action :check_if_logged_in, except: [:index]
+  before_action :check_if_logged_in, except: [:index]
   before_action :get_chatroom, only: [:show, :add_user]
 
   def add_user
